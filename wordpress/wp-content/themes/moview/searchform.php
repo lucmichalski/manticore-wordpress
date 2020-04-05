4e096d1654ade31f5f06490804a5c36445416dfc
lucmichalski <luc.michalski@protonmail.com>
2020-04-05 09:55:47 +0000

    cleanup

--- wordpress/wp-content/themes/moview/searchform.php
+++
@@ -1,4 +0,0 @@
-<form method="get" id="searchform" action="<?php echo esc_url(home_url( '/' )); ?>" >
-    <input type="text" value="" name="s" id="s" class="form-control" placeholder="<?php esc_html_e('Search . . . . .','moview'); ?>" autocomplete="off" />
-    <button class="btn btn-primary btn-style btn-search"><i class="fa fa-search"></i></button>
-</form>
